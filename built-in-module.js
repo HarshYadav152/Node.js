be5084e9c85aec2aa6d147800dfe14582cc10112
Harsh Yadav <161936085+HarshYadav152@users.noreply.github.com>
2024-05-06 12:07:40 +0530

    Revert "Node.js basics"

--- built-in-module.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-let text = fs.readFileSync("E:\\Programming\\Web development\\Java Script\\NodeJS\\harsh.txt","utf-8");
-
-text = text.replace("hello","Harsh");
-console.log(text);
-
-console.log("Creating a new file");
-fs.writeFileSync("Yadav.txt",text);
--- third-party-module.js
+++
@@ -1,3 +0,0 @@
-console.log("Express ,React And Mongoose are example of 3rd party module ");
-console.log("These can be installed using npm(node package manager");
-console.log("npm install module_name");
